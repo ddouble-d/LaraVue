99d46d87ae4cadd32a60b24f443b25f757d1ec71
Double D <adeth.slipz@gmail.com>
2020-09-04 00:58:50 +0800

    adjust profle

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -1,11 +1,148 @@
 <template>
   <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
+    <div class="row">
+      <div class="col-md-12 mt-3">
+        <!-- Widget: user widget style 1 -->
+        <div class="card card-widget widget-user">
+          <!-- Add the bg color to the header using any of the bg-* classes -->
+          <div
+            class="widget-user-header text-white"
+            style="background-image:url('./img/user-cover.png')"
+          >
+            <h3 class="widget-user-username text-right">Elizabeth Pierce</h3>
+            <h5 class="widget-user-desc text-right">Web Designer</h5>
+          </div>
+          <div class="widget-user-image">
+            <img
+              class="img-circle"
+              src="https://adminlte.io/themes/v3/dist/img/user3-128x128.jpg"
+              alt="User Avatar"
+            />
+          </div>
+          <div class="card-footer">
+            <div class="row">
+              <div class="col-sm-4 border-right">
+                <div class="description-block">
+                  <h5 class="description-header">3,200</h5>
+                  <span class="description-text">SALES</span>
+                </div>
+                <!-- /.description-block -->
+              </div>
+              <!-- /.col -->
+              <div class="col-sm-4 border-right">
+                <div class="description-block">
+                  <h5 class="description-header">13,000</h5>
+                  <span class="description-text">FOLLOWERS</span>
+                </div>
+                <!-- /.description-block -->
+              </div>
+              <!-- /.col -->
+              <div class="col-sm-4">
+                <div class="description-block">
+                  <h5 class="description-header">35</h5>
+                  <span class="description-text">PRODUCTS</span>
+                </div>
+                <!-- /.description-block -->
+              </div>
+              <!-- /.col -->
+            </div>
+            <!-- /.row -->
+          </div>
+        </div>
+        <!-- /.widget-user -->
+      </div>
+      <div class="col-md-12 mt-3">
         <div class="card">
-          <div class="card-header">Profile Component</div>
+          <div class="card-header p-2">
+            <ul class="nav nav-pills">
+              <li class="nav-item">
+                <a class="nav-link active" href="#activity" data-toggle="tab">Activity</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#settings" data-toggle="tab">Settings</a>
+              </li>
+            </ul>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <div class="tab-content">
+              <div class="tab-pane active" id="activity">
+                <!-- Post -->
+                <div class="post">
+                  <h1>Display User Activity</h1>
+                  <!-- /.post -->
+                </div>
+              </div>
 
-          <div class="card-body">I'm an Profile component.</div>
+              <div class="tab-pane" id="settings">
+                <form class="form-horizontal">
+                  <div class="form-group row">
+                    <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                    <div class="col-sm-10">
+                      <input
+                        type="text"
+                        v-model="form.name"
+                        class="form-control"
+                        id="inputName"
+                        placeholder="Name"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                      <input
+                        type="email"
+                        v-model="form.email"
+                        class="form-control"
+                        id="inputEmail"
+                        placeholder="Email"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="inputExperience" class="col-sm-2 col-form-label">Experience</label>
+                    <div class="col-sm-10">
+                      <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="inputPhoto" class="col-sm-2 col-form-label">Profile Photo</label>
+                    <div class="input-group col-sm-10">
+                      <div class="custom-file">
+                        <input
+                          type="file"
+                          @change="updateProfile"
+                          class="custom-file-input"
+                          id="inputPhoto"
+                        />
+                        <label class="custom-file-label" for="inputPhoto">Choose file</label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="inputPassport" class="col-sm-2 col-form-label">Passport (Optional)</label>
+                    <div class="col-sm-10">
+                      <input
+                        type="password"
+                        class="form-control"
+                        id="inputPassport"
+                        placeholder="Passport"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <div class="offset-sm-2 col-sm-10">
+                      <button type="submit" class="btn btn-danger">Submit</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <!-- /.tab-pane -->
+            </div>
+            <!-- /.tab-content -->
+          </div>
+          <!-- /.card-body -->
         </div>
       </div>
     </div>
@@ -14,8 +151,42 @@
 
 <script>
 export default {
-  mounted() {
-    console.log("Component mounted.");
+  data() {
+    return {
+      form: new Form({
+        id: "",
+        name: "",
+        email: "",
+        password: "",
+        type: "",
+        bio: "",
+        photo: "",
+      }),
+    };
+  },
+  methods: {
+    updateProfile(e) {
+      // console.log("uploaded");
+      let file = e.target.files[0];
+      let reader = new FileReader();
+      reader.onloadend = (file) => {
+        // console.log("RESULT", reader.result);
+        this.form.photo = reader.result;
+      };
+      reader.readAsDataURL(file);
+    },
+  },
+  mounted() {},
+  created() {
+    axios.get("api/profile").then(({ data }) => this.form.fill(data));
   },
 };
 </script>
+
+<style scoped>
+.widget-user-header {
+  background-position: center center;
+  background-size: cover;
+  height: 250px;
+}
+</style>
